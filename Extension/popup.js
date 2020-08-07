723a867f3dcf552fa5acaebacfb392d107b81992
HigorLoren <higor.lorenzon@gmail.com>
2020-08-07 17:18:27 -0300

    First working version

--- Extension/popup.js
+++ Extension/popup.js
@@ -1,3 +1,62 @@
+const saveNewSession = newSessionName => {
+  document.getElementById("saveNewSession").style.display = "none";
+  document.getElementById("newSessionName").value = "";
+
+  chrome.tabs.query({currentWindow: true, windowType: "normal"}, tabs => {
+    let tabsForSave = tabs.map(tab => ({
+      id: String(tab.id),
+      title: tab.title,
+      url: tab.url,
+      favIconUrl: tab.favIconUrl ? tab.favIconUrl : ""
+    }));
+
+    chrome.storage.sync.get(["sessions"], (storage) => {
+      let newSessions = [];
+
+      if (storage.sessions !== undefined) {
+        newSessions = storage.sessions.length > 0 ? storage.sessions : [];
+      }
+
+      newSessions.push({ id: Math.random().toString(16).slice(2), name: newSessionName, tabsCount: tabsForSave.length, tabs: tabsForSave });
+      chrome.storage.sync.set({ sessions: newSessions }, renderSessions);
+    });
+  });
+};
+
+const openSession = e => {
+  e.preventDefault();
+
+  let sessionContent = e.currentTarget || e.target; 
+
+  chrome.storage.sync.get(["sessions"], storage => {
+    let session = storage.sessions.find(session => session.id === sessionContent.getAttribute("session_id"));
+
+    chrome.tabs.query({currentWindow: true}, tabs => {
+      if (tabs.length === 1) {
+        if (tabs[0].url == "chrome://newtab/") {
+          chrome.tabs.remove(tabs[0].id);
+
+          session.tabs.forEach(tab => {
+            chrome.tabs.create({url: tab.url, active: false});
+          });
+        } else {
+          chrome.windows.create({focused: true, state: "maximized", url: session.tabs.map(tab => tab.url)});
+        }
+      } else {
+        chrome.windows.create({focused: true, state: "maximized", url: session.tabs.map(tab => tab.url)});
+      }
+    });
+  });
+};
+
+const deleteSession = session_id => {
+  chrome.storage.sync.get(["sessions"], storage => {
+    chrome.storage.sync.set({
+      sessions: storage.sessions.filter(sessionFilter => sessionFilter.id !== session_id)
+    }, renderSessions);
+  });
+};
+
 const renderSessions = () => {
   chrome.storage.sync.get(["sessions"], storage => {
     document.getElementById("sessions").innerHTML = "";
@@ -6,12 +65,10 @@ const renderSessions = () => {
       storage.sessions.forEach(session => {
         let divSession = document.createElement("div");
         divSession.classList.add("session");
-        divSession.setAttribute("links", JSON.stringify(session.tabs));
         divSession.setAttribute("id", session.id);
-        divSession.setAttribute("name", session.name);
 
         let divSessionContent = document.createElement("div");
-        divSessionContent.onclick = openSessionTabs;
+        divSessionContent.addEventListener("click", e => openSession(e), true);
         divSessionContent.setAttribute("session_id", session.id);
         divSessionContent.classList.add("content");
 
@@ -28,8 +85,19 @@ const renderSessions = () => {
 
         let divSessionActions = document.createElement("div");
         divSessionActions.classList.add("actions");
-        divSessionActions.appendChild(createDeleteIcon(session.id));
-        divSessionActions.appendChild(createMoreIcon(session.id));
+
+        let divIconDelete = document.createElement("div");
+        divIconDelete.classList.add("deleteIcon");
+        divIconDelete.classList.add("icon");
+        divIconDelete.addEventListener("click", () => deleteSession(session.id));
+        divSessionActions.appendChild(divIconDelete);
+
+        let divIconMore = document.createElement("div");
+        divIconMore.classList.add("moreIcon");
+        divIconMore.classList.add("icon");
+        divIconMore.addEventListener("click", () => renderTabs(session.id));
+        divSessionActions.appendChild(divIconMore);
+
         divSession.appendChild(divSessionActions);
 
         document.getElementById("sessions").appendChild(divSession);
@@ -38,154 +106,110 @@ const renderSessions = () => {
   });
 };
 
-document.addEventListener("DOMContentLoaded", renderSessions);
+const renderTabs = session_id => {
+  chrome.storage.sync.get(["sessions"], storage => {
+    let session = storage.sessions.find(session => session.id === session_id);
+  
+    document.getElementById("selectedSessionName").value = session.name;
+    document.getElementById("selectedSessionName").setAttribute("session_id", session_id);
 
-document.getElementById("addSession").addEventListener("click", e => {
-  e.preventDefault();
-
-  if (document.getElementById("newSession").style.display === "none") {
-    document.getElementById("newSession").style.display = "flex";
-    document.getElementById("inputNewSession").focus();
-  }
-});
-
-document.getElementById("inputNewSession").addEventListener("keypress", e => {
-  if (e.key === 'Enter') {
-    e.preventDefault;
-    storageCurrentTabs(e.target.value);
-  }
-});
-
-document.getElementById("btnNewSession").addEventListener("click", e => {
-  e.preventDefault;
-  storageCurrentTabs(document.getElementById("inputNewSession").value);
-});
-
-document.getElementById("btnBackSessions").addEventListener("click", e => {
-  document.getElementById("sessions-page").classList.add("show");
-  document.getElementById("links-page").classList.remove("show");
-  document.getElementById("linksList").innerHTML = "";
-});
-
-document.getElementById("openSessionName").addEventListener("focus", e => e.target.select());
-
-document.getElementById("openSessionName").addEventListener("blur", e => {
-  chrome.storage.sync.get(["sessions"], (storage) => {
-    let session_id = e.target.getAttribute("session_id");
-    let newName = e.target.textContent ? e.target.textContent : e.target.innerText;
-
-    let newSessions = storage.sessions.map(session => ({
-      id: session.id,
-      name: (session.id == session_id) ? newName : "batata",
-      tabsCount: session.tabsCount,
-      tabs: session.tabs
-    }));
-
-    console.dir(newSessions);
-
-    chrome.storage.sync.set({ sessions: newSessions }, renderSessions);
-  });
-});
-
-const storageCurrentTabs = (newSession) => {
-  document.getElementById("newSession").style.display = "none";
-  document.getElementById("inputNewSession").value = "";
-
-  chrome.tabs.query({currentWindow: true, windowType: "normal"}, tabs => {
-    let tabsForSave = tabs.map(tab => ({id: tab.id, title: tab.title, url: tab.url}));
-
-    chrome.storage.sync.get(["sessions"], (storage) => {
-      let newSessions = [];
-
-      if (storage.sessions !== undefined) {
-        newSessions = storage.sessions.length > 0 ? storage.sessions : [];
-      }
-
-      newSessions.push({ id: Math.random().toString(16).slice(2), name: newSession, tabsCount: tabsForSave.length, tabs: tabsForSave });
-      chrome.storage.sync.set({ sessions: newSessions }, renderSessions);
-    });
-  });
-};
-
-const openSessionTabs = (e) => {
-  e.preventDefault();
-
-  let urls = document.querySelectorAll(`.session[session_id='${e.currentTarget.getAttribute("session_id")}']`)[0].getAttribute("links");
-  urls = JSON.parse(urls).map(link => (link.url));
-
-  chrome.tabs.query({currentWindow: true}, (tabs) => {
-    if (tabs.length === 1) {
-      if (tabs[0].url == "chrome://newtab/") {
-        chrome.tabs.remove(tabs[0].id);
-
-        JSON.parse(urls).forEach(link => {
-          chrome.tabs.create({url: link, active: false});
-        });
-      } else {
-        chrome.windows.create({focused: true, state: "maximized", url: urls});
-      }
-    } else {
-      chrome.windows.create({focused: true, state: "maximized", url: urls});
-    }
-  });
-}
-
-const createDeleteIcon = session_id => {
-  let divIconDelete = document.createElement("div");
-  divIconDelete.classList.add("deleteIcon");
-  divIconDelete.setAttribute("session_id", session_id);
-  divIconDelete.addEventListener("click", e => {
-    chrome.storage.sync.get(["sessions"], storage => {
-      newSessions = storage.sessions.length > 0 ? storage.sessions : [];
-
-      newSessions = newSessions.filter(sessionFilter => {
-        return sessionFilter.id !== e.target.getAttribute("session_id");
-      });
-
-      chrome.storage.sync.set({ sessions: newSessions }, renderSessions);
-    });
-  }, false);
-
-  return divIconDelete;
-}
-
-const createMoreIcon = session_id => {
-  let divIconMore = document.createElement("div");
-  divIconMore.classList.add("moreIcon");
-  divIconMore.setAttribute("session_id", session_id);
-
-  divIconMore.addEventListener("click", e => {
-    document.getElementById("linksList").innerHTML = "";
-    document.getElementById("links-page").classList.add("show");
+    document.getElementById("tabs").innerHTML = "";
+    document.getElementById("tabs-page").classList.add("show");
     document.getElementById("sessions-page").classList.remove("show");
 
-    let session = document.querySelectorAll(`.session[id='${e.target.getAttribute("session_id")}']`)[0]
-    let links= JSON.parse(session.getAttribute("links"))
-    let sessionName = session.getAttribute("name")
-
-    document.getElementById("openSessionName").textContent = sessionName;
-    document.getElementById("openSessionName").setAttribute("session_id", session_id);
-    links.forEach(link => {
+    session.tabs.forEach(tab => {
       let div = document.createElement("div");
-      div.classList.add("linkDiv");
+      div.setAttribute("id", tab.id);
+      div.classList.add("tab");
 
       let a = document.createElement("a");
       a.classList.add("bold");
-      a.href = "#";
-      a.addEventListener(
-        "click",
-        function (e) {
-          e.preventDefault();
-          chrome.tabs.create({url: link.url, active: true});
-        },
-        false
-      );
-      a.textContent = link.url.replace("https://", "").replace("http://", "").replace(/\/$/, "");
+      a.href = tab.url;
+      a.addEventListener("click", e => {
+        e.preventDefault();
+        chrome.tabs.create({url: tab.url, active: true});
+      });
+      a.textContent = tab.title;
+
+      let favicon = document.createElement("div");
+      favicon.classList.add("icon");
+      favicon.classList.add("favicon");
+      favicon.style.backgroundImage = `url(${tab.favIconUrl}`;
+      a.prepend(favicon);
+
       div.appendChild(a);
 
-      document.getElementById("linksList").appendChild(div);
+      let divIconDeletetab = document.createElement("div");
+      divIconDeletetab.classList.add("deleteIcon");
+      divIconDeletetab.classList.add("icon");
+      divIconDeletetab.addEventListener("click", () => deleteTab(tab.id, session_id));
+      div.appendChild(divIconDeletetab);
+
+      document.getElementById("tabs").appendChild(div);
     });
   });
-
-  return divIconMore;
 }
+
+const deleteTab = (tab_id, session_id) => {
+  chrome.storage.sync.get(["sessions"], storage => {
+    let newSessions = storage.sessions
+    let sessionIndex = newSessions.findIndex(session => session.id === session_id);
+
+    newSessions[sessionIndex].tabs = newSessions[sessionIndex].tabs.filter(tab => tab.id !== tab_id)
+    newSessions[sessionIndex].tabsCount -= 1;
+
+    document.querySelectorAll(`.tab[id='${tab_id}']`)[0].remove()
+
+    chrome.storage.sync.set({ sessions: newSessions }, renderSessions);
+  });
+};
+
+document.addEventListener("DOMContentLoaded", renderSessions);
+
+document.getElementById("btnAddSession").addEventListener("click", e => {
+  e.preventDefault();
+
+  if (document.getElementById("saveNewSession").style.display === "none") {
+    document.getElementById("saveNewSession").style.display = "flex";
+    document.getElementById("newSessionName").focus();
+  } else {
+    document.getElementById("saveNewSession").style.display = "none";
+  }
+});
+
+document.getElementById("newSessionName").addEventListener("keypress", e => {
+  if (e.key === 'Enter') {
+    e.preventDefault;
+    saveNewSession(e.target.value);
+  }
+});
+
+document.getElementById("btnSaveNewSession").addEventListener("click", e => {
+  e.preventDefault;
+  saveNewSession(document.getElementById("newSessionName").value);
+});
+
+document.getElementById("btnBackToSessions").addEventListener("click", e => {
+  document.getElementById("sessions-page").classList.add("show");
+  document.getElementById("tabs-page").classList.remove("show");
+  document.getElementById("tabs").innerHTML = "";
+});
+
+document.getElementById("selectedSessionName").addEventListener("focus", e => e.target.select());
+
+document.getElementById("selectedSessionName").addEventListener("keypress", e => {
+  if (e.key === 'Enter') {
+    e.preventDefault;
+    e.target.blur();
+  }
+});
+
+document.getElementById("selectedSessionName").addEventListener("blur", e => {
+  chrome.storage.sync.get(["sessions"], (storage) => {
+    let sessionIndex = storage.sessions.findIndex(session => session.id === e.target.getAttribute("session_id"));
+
+    storage.sessions[sessionIndex].name = e.target.value;
+
+    chrome.storage.sync.set({ sessions: storage.sessions }, renderSessions);
+  });
+});
